3bbf07384095b3159b05ceaebce1ed057a24d7dc
Ben Keller <malzraa@gmail.com>
2025-06-15 23:28:42 -0500

    Show age of snapshot in movie

--- make_frames.py
+++ make_frames.py
@@ -13,12 +13,14 @@ if __name__ == "__main__":
         print(f"Processing Frame {i+1} of {n_snaps}")
         snap = pyn.load(f'{argv[1]}/{basename}.{i+1:05d}')
         plt.clf()
-        plt.plot(snap.s['x'][snap.s['tform'] == 1], snap.s['y'][snap.s['tform'] == 1], 'c,', alpha=0.2)
-        plt.plot(snap.s['x'][snap.s['tform'] == 0], snap.s['y'][snap.s['tform'] == 0], 'y,', alpha=0.05)
+        young = snap.s[snap.s['tform'] == 1]
+        old = snap.s[snap.s['tform'] == 0]
+        plt.plot(young['x'][np.argsort(young['z'])], young['y'][np.argsort(young['z'])], 'c,', alpha=0.2)
+        plt.plot(old['x'][np.argsort(old['z'])], old['y'][np.argsort(old['z'])], 'y,', alpha=0.05)
         plt.ylim(-150, 150)
         plt.xlim(-150, 150)
         plt.gca().set_facecolor('black')
         plt.gca().set_aspect('equal')
         plt.gcf().set_facecolor('black')
-        plt.figtext(0.9, 0.9, f'{snap.properties["time"].in_units("Myr"):%4d} Myr', color='white')
+        plt.figtext(0.8, 0.9, f'{int(snap.properties["time"].in_units("Myr"))} Myr', color='white')
         plt.savefig(f'images/im{i:05d}.png', dpi=200)
